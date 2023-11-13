3f7cb6c0fe6aefc8d6a5b18fbd0de6ffe847e638
affanansari7861 <105743264+affanansari7861@users.noreply.github.com>
2023-11-13 15:02:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({ useTempFiles: true }));
 app.use(cors());
-// app.use(express.static("./public"));
+app.use(express.static("./public"));
 
 // routes
 app.use("/user", userRouter);
