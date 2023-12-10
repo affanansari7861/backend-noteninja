2be6c9305b9e59df98e73d7e1e4859452a15d0b8
affanansari7861 <105743264+affanansari7861@users.noreply.github.com>
2023-12-10 14:48:04 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,7 @@ const LocalStrategy = require("passport-local").Strategy;
 const GTIHUBSTRATEGY = require("passport-github2").Strategy;
 
 passport.serializeUser((user, done) => {
+  console.log(user);
   done(null, user._id);
 });
 passport.deserializeUser(async (id, done) => {
