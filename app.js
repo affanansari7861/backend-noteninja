bdcda503c73e4c439ccb976b70519fda81949c2b
affanansari7861 <105743264+affanansari7861@users.noreply.github.com>
2023-12-12 07:26:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(
   session({
     secret: "asdf",
     resave: true,
-    saveUninitialized: true,
+    saveUninitialized: false,
     // maxAge: 1000 * 60 * 60 * 60 * 24 * 7,
     name: "token",
     cookie: {
